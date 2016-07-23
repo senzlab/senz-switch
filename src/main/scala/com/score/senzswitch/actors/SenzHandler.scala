53ebf48d31690654b2313a375ef5f1e413a4ddb2
eranga herath <erangaeb@gmail.com>
2016-07-23 17:41:42 +0530

    handle different senz types

--- src/main/scala/com/score/senzswitch/actors/SenzHandler.scala
+++ src/main/scala/com/score/senzswitch/actors/SenzHandler.scala
@@ -3,6 +3,7 @@ package com.score.senzswitch.actors
 import akka.actor.{Actor, ActorRef, Props}
 import akka.io.Tcp
 import akka.util.ByteString
+import com.score.senzswitch.config.Configuration
 import com.score.senzswitch.protocols.{Senz, SenzMsg, SenzType}
 import com.score.senzswitch.utils.SenzParser
 import org.slf4j.LoggerFactory
@@ -10,13 +11,11 @@ import org.slf4j.LoggerFactory
 
 object SenzHandler {
 
-  def props(senderRef: ActorRef): Props = {
-    Props(new SenzHandler(senderRef))
-  }
+  def props(senderRef: ActorRef) = Props(new SenzHandler(senderRef))
 
 }
 
-class SenzHandler(senderRef: ActorRef) extends Actor {
+class SenzHandler(senderRef: ActorRef) extends Actor with Configuration {
 
   def logger = LoggerFactory.getLogger(this.getClass)
 
@@ -37,22 +36,59 @@ class SenzHandler(senderRef: ActorRef) extends Actor {
       val senz = SenzParser.parse(senzMsg.data)
       senz match {
         case Senz(SenzType.SHARE, sender, receiver, attr, signature) =>
-          logger.info("SHARE from " + sender + " to " + receiver)
-          SenzListener.actorRefs.put(sender, self)
+          handleShare(senz, senzMsg)
         case Senz(SenzType.PING, sender, receiver, attr, signature) =>
-          logger.info("PING from " + sender + " to " + receiver)
-          SenzListener.actorRefs.put(sender, self)
-        case Senz(senzType, sender, receiver, attr, signature) =>
-          logger.info("Senz " + senzType.toString + " from " + sender + " to " + receiver)
-          SenzListener.actorRefs.get(receiver).get ! senzMsg
+          handlePing(senz)
+        case Senz(SenzType.GET, sender, receiver, attr, signature) =>
+          handleGet(senz, senzMsg)
+        case Senz(SenzType.DATA, sender, receiver, attr, signature) =>
+          handleData(senz, senzMsg)
+        case Senz(SenzType.PUT, sender, receiver, attr, signature) =>
+          handlePut(senz, senzMsg)
       }
     case Tcp.PeerClosed =>
       logger.info("Peer Closed")
       context stop self
-    case Senz(senzType, sender, receiver, attr, signature) =>
-      senderRef ! Tcp.Write(ByteString(s"$senzType $sender $receiver $signature\n\r"))
     case SenzMsg(data) =>
       senderRef ! Tcp.Write(ByteString(s"$data\n\r"))
+  }
+
+  def handleShare(senz: Senz, senzMsg: SenzMsg) = {
+    if (senz.receiver.equalsIgnoreCase(switchName)) {
+      // public key sharing
+      // store actor
+      SenzListener.actorRefs.put(senz.sender, self)
+    } else {
+      // share senz for other senzie
+      // forward senz to receiver
+      SenzListener.actorRefs.get(senz.receiver).get ! senzMsg
+    }
+  }
+
+  def handleGet(senz: Senz, senzMsg: SenzMsg) = {
+    if (senz.receiver.equalsIgnoreCase(switchName)) {
+      // most of the time request public key of other senzie
+      // TODO handle it
+    } else {
+      // get senz for other senzie
+      // forward senz to receiver
+      SenzListener.actorRefs.get(senz.receiver).get ! senzMsg
+    }
+  }
+
+  def handlePing(senz: Senz) = {
+    // store/restore actor
+    SenzListener.actorRefs.put(senz.sender, self)
+  }
+
+  def handleData(senz: Senz, senzMsg: SenzMsg) = {
+    // forward senz to receiver
+    SenzListener.actorRefs.get(senz.receiver).get ! senzMsg
+  }
+
+  def handlePut(senz: Senz, senzMsg: SenzMsg) = {
+    // forward senz to receiver
+    SenzListener.actorRefs.get(senz.receiver).get ! senzMsg
   }
 
 }
