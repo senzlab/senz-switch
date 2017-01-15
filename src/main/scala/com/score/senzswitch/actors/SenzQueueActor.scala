f2fbca16b2738cb3a1b1d78709c069298199156c
eranga herath <eranga.herath@pagero.com>
2017-01-15 16:08:01 +0530

    handle queue for miss call

--- src/main/scala/com/score/senzswitch/actors/SenzQueueActor.scala
+++ src/main/scala/com/score/senzswitch/actors/SenzQueueActor.scala
@@ -2,7 +2,9 @@ package com.score.senzswitch.actors
 
 import akka.actor.{Actor, ActorRef, Props}
 import com.score.senzswitch.protocols.{Msg, SenzMsg}
+import com.score.senzswitch.utils.SenzParser
 import org.slf4j.LoggerFactory
+import sun.misc.BASE64Encoder
 
 object SenzQueueActor {
 
@@ -43,25 +45,31 @@ class SenzQueueActor extends Actor {
         senzQueue.find(obj => matchSenderReceiver(obj, qObj) && obj.senzMsg.senz.attributes.contains("#cam")) match {
           case Some(obj) =>
           case _ =>
-            enqueueObj(qObj)
+            val oriMsg = new BASE64Encoder().encode(qObj.senzMsg.data.getBytes).replaceAll("\n", "").replaceAll("\r", "")
+            val msg = s"DATA #senz $oriMsg @${qObj.senzMsg.senz.receiver} #uid${qObj.uid} ^senzswitch DIGSIG"
+            val senz = SenzParser.parseSenz(msg)
+            enqueueObj(QueueObj(qObj.uid, SenzMsg(senz, msg)))(qObj.senzMsg.senz.sender)
         }
       } else if (qObj.senzMsg.senz.attributes.contains("#mic")) {
         // only keep one #mic message which correspond for receiver and sender in queue
         senzQueue.find(obj => matchSenderReceiver(obj, qObj) && obj.senzMsg.senz.attributes.contains("#mic")) match {
           case Some(obj) =>
           case _ =>
-            enqueueObj(qObj)
+            val oriMsg = new BASE64Encoder().encode(qObj.senzMsg.data.getBytes).replaceAll("\n", "").replaceAll("\r", "")
+            val msg = s"DATA #senz $oriMsg @${qObj.senzMsg.senz.receiver} #uid${qObj.uid} ^senzswitch DIGSIG"
+            val senz = SenzParser.parseSenz(msg)
+            enqueueObj(QueueObj(qObj.uid, SenzMsg(senz, msg)))(qObj.senzMsg.senz.sender)
         }
       } else if (qObj.senzMsg.senz.attributes.contains("#lat")) {
         // only keep one #lat message which correspond for receiver and sender in queue
         senzQueue.find(obj => matchSenderReceiver(obj, qObj) && qObj.senzMsg.senz.attributes.contains("#lat")) match {
           case Some(obj) =>
           case _ =>
-            enqueueObj(qObj)
+            enqueueObj(qObj)()
         }
       } else if (qObj.senzMsg.senz.attributes.contains("#msg") || qObj.senzMsg.senz.attributes.contains("$msg")) {
         // keep all #msg messages
-        enqueueObj(qObj)
+        enqueueObj(qObj)()
       }
     case Dequeue(uid) =>
       logger.debug(s"Dequeue with uid $uid")
@@ -72,8 +80,11 @@ class SenzQueueActor extends Actor {
           senzQueue -= qObj
 
           // send DELIVERED status back to sender
-          val payload = s"DATA #status DELIVERED #uid ${qObj.uid} @${qObj.senzMsg.senz.sender} ^senzswitch SIGNATURE"
-          SenzListenerActor.actorRefs(qObj.senzMsg.senz.sender).actorRef ! Msg(payload)
+          // for calls/selfies we don't send status back
+          if (!qObj.senzMsg.senz.attributes.contains("#senz")) {
+            val payload = s"DATA #status DELIVERED #uid ${qObj.uid} @${qObj.senzMsg.senz.sender} ^senzswitch SIGNATURE"
+            SenzListenerActor.actorRefs(qObj.senzMsg.senz.sender).actorRef ! Msg(payload)
+          }
         case None =>
           // no matcher
           logger.debug(s"no matching obj for uid - $uid")
@@ -85,12 +96,12 @@ class SenzQueueActor extends Actor {
       senzQueue.filter(qObj => qObj.senzMsg.senz.receiver.equalsIgnoreCase(user)).foreach(s => actorRef ! Msg(s.senzMsg.data))
   }
 
-  private def enqueueObj(qObj: QueueObj) = {
+  private def enqueueObj(qObj: QueueObj)(sender: String = qObj.senzMsg.senz.sender) = {
     senzQueue += qObj
 
     // send RECEIVED status back to sender
     val payload = s"DATA #status RECEIVED #uid ${qObj.uid} @${qObj.senzMsg.senz.sender} ^senzswitch SIGNATURE"
-    SenzListenerActor.actorRefs(qObj.senzMsg.senz.sender).actorRef ! Msg(payload)
+    SenzListenerActor.actorRefs(sender).actorRef ! Msg(payload)
   }
 
   private def matchSenderReceiver(qObj1: QueueObj, qObj2: QueueObj) = {
--- src/main/scala/com/score/senzswitch/utils/SenzUtils.scala
+++ src/main/scala/com/score/senzswitch/utils/SenzUtils.scala
@@ -6,6 +6,6 @@ package com.score.senzswitch.utils
 object SenzUtils {
   def getPingSenz(receiver: String, sender: String) = {
     val timestamp = (System.currentTimeMillis / 1000).toString
-    s"PING #time ${timestamp} @$receiver ^$sender"
+    s"PING #time $timestamp @$receiver ^$sender"
   }
 }
